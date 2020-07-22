7f7f5d5c3bd089c4ce54c9fe165c9dcc586a75ec
Tom Hall <tomhall.main@gmail.com>
2020-07-22 09:34:03 -0500

    Added piped stdin support to awk_col

--- .commands.sh
+++ .commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 lbv() { # Generates a cross table of git repos vs branches
-  if [ -z $1 ] ; then
+  if [ -z $1 ]; then
     bash ~/dev_scripts/scripts/local_branch_view.sh
   else
     local flags="${@}"
@@ -26,19 +26,19 @@ git_branch() { # Runs git branch for all repos
 }
 
 gc() { # git commit
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   local args=$@
   git commit "$args"
 }
 
 gcam() { # git commit -am 'commit message'
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   local COMMIT_MESSAGE="$1"
   git commit -am "$COMMIT_MESSAGE"
 }
 
 gadd() { # Adds all untracked git files
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   local ALL_FILES=$(git ls-files -o --exclude-standard)
   if [ -z $ALL_FILES ]; then
     echo 'No untracked files found to add'
@@ -48,13 +48,13 @@ gadd() { # Adds all untracked git files
 }
 
 gpcurr() { # git push origin for current branch
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   git push origin "$CURRENT_BRANCH"
 };
 
 gacmp() { # Adds all untracked files, commits with messsage arg, pushes current branch
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   gadd
   local COMMIT_MESSAGE="$1"
   gcam "$COMMIT_MESSAGE"
@@ -62,14 +62,14 @@ gacmp() { # Adds all untracked files, commits with messsage arg, pushes current 
 }
 
 git_recent() { # Display a table of commits sorted by most recent descending
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   git for-each-ref --sort=-committerdate refs/heads \
     --format='%(HEAD)%(color:yellow)%(refname:short)|%(color:bold green)%(committerdate:relative)|%(color:blue)%(subject)|%(color:magenta)%(authorname)%(color:reset)' \
     --color=always | column -ts '|'
 }
 
 git_graph() { # Prints colorful git history graph
-  if not_git; then return; fi
+  if not_git; then return 1; fi
   git log --all --decorate --oneline --graph
 }
 
@@ -82,6 +82,10 @@ not_git() { # Checks if the current directory is not part of a git repo
   fi
 }
 
+data_in() { # Detects if data is being received in stdin via a pipe
+  [ -p /dev/stdin ]
+}
+
 join_by() { # Joins a shell array by a text argument provided
   local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}";
 }
@@ -91,6 +95,7 @@ dup_in_dir() { # Reports duplicate files and gives option for deletion
 }
 
 todo() { # Lists all todo items found in current directory
+  # TODO: Add support for multiple dirs
   # local paths=( "${@}" )
   #for path in "${paths[@]}"; do
   #  if [ ! -d path ]; then
@@ -111,13 +116,23 @@ rgtodo() { # Lists all todo items found in current dir using ripgrep if installe
 
 awk_col() { # Prints field-separated data in columns with dynamic width
   local args=( "$@" )
-  local args_len=${#args[@]}
-  let last_arg=$args_len-1
-  local file="${args[@]:$last_arg:1}"
   COL_MARGIN=${COL_MARGIN:-1} # Set an envvar for margin between cols, default is 1 char 
-  args=( ${args[@]/"$file"} )
+  if data_in; then
+    local file=/tmp/awkcol_showlater piped=0
+    cat /dev/stdin > $file
+  else
+    local args_len=${#args[@]}
+    let last_arg=$args_len-1
+    local file="${args[@]:$last_arg:1}"
+    args=( ${args[@]/"$file"} )
+  fi
   awk -f ~/dev_scripts/scripts/max_field_lengths.awk \
     -v buffer=$COL_MARGIN ${args[@]} "$file" "$file"
+  if [ $piped ]; then rm $file &> /dev/null; fi
+}
+
+duplicate_input() { # Duplicates input sent to stdin in aggregate
+  tee /tmp/showlater && cat /tmp/showlater && rm /tmp/showlater
 }
 
 ls_commands() { # Lists commands in the dev_scripts/.commands.sh file
